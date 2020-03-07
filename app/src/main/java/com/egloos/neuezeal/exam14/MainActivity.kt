e715f8f48a601e6c0b1baab2c36ce64baf16e575
leeyunhome <leeyunhome@daum.net>
2020-03-07 13:46:34 +0900

    Initial commit

---
+++ app/src/main/java/com/egloos/neuezeal/exam14/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.egloos.neuezeal.exam14
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webButton.setOnClickListener(View.OnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://google.com/"))
+            startActivity(intent)
+        })
+
+        sendButton.setOnClickListener(View.OnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_SUBJECT, "제목")
+            intent.putExtra(Intent.EXTRA_TEXT, "공유할 내용입니다")
+
+            startActivity(intent)
+        })
+    }
+}
